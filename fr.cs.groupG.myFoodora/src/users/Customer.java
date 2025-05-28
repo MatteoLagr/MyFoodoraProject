a0832035593e2f7cc4d150d8e1439e8bd6634645
matte <matte@Matteo_L>
2025-05-29 00:00:59 +0200

    test Customer class

--- fr.cs.groupG.myFoodora/src/users/Customer.java
+++ fr.cs.groupG.myFoodora/src/users/Customer.java
@@ -1,5 +1,31 @@
 package users;
 
-public class Customer {
+import java.util.ArrayList;
+
+
+public class Customer extends Users{
+	
+	private Point2D address;
+	private String email;
+	private String phoneNumber;
+	private FidelityCard fidelityCard;
+	private List<Order> orderHistory;
+	private boolean notificationConsent;
+	private int fidelityPoints;
+	
+	
+	public Customer(String name, String username, String surname, int id, Point2D address, String email, String phoneNumber) {
+		super(name, username, surname, id, "");
+		this.address=address;
+		this.email=email;
+		this.phoneNumber=phoneNumber;
+		this.orderHistory = new ArrayList<>();
+		this.notificationConsent = false;
+		this.fidelityPoints = 0;
+	}
+	
+	
+	
+	
 
 }
