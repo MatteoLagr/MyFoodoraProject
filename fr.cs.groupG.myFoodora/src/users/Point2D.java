d06a30246177323b03153246888af50192193797
matte <matte@Matteo_L>
2025-05-28 23:14:35 +0200

    classe Point2D

--- fr.cs.groupG.myFoodora/src/users/Point2D.java
+++ fr.cs.groupG.myFoodora/src/users/Point2D.java
@@ -1,5 +1,37 @@
 package users;
 
 public class Point2D {
+	
+	private double x;
+	private double y;
+	
+	
+	public Point2D(double x, double y) {
+		this.x=x;
+		this.y=y;
+	}
+	
+	public double getX() {
+		return x;
+	}
 
+	public double getY() {
+		return y;
+	}
+	
+	public void setX(double x) {
+		this.x=x;
+	}
+	
+	public void setY(double y) {
+		this.y=y;
+	}
+	
+	public double distanceTo(Point2D point) {
+		double dx = this.x - point.x;
+		double dy = this.y - point.y;
+		return Math.sqrt(dx * dx + dy * dy);
+	}
+	
+	
 }
