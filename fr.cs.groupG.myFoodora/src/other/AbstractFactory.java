daa735a5b2e18453307c9655e90ca4118fac261b
matte <matte@Matteo_L>
2025-05-29 10:30:52 +0200

    Modiff AbstractFactory UsersFactory

--- fr.cs.groupG.myFoodora/src/other/AbstractFactory.java
+++ fr.cs.groupG.myFoodora/src/other/AbstractFactory.java
@@ -1,6 +1,18 @@
 package other;
 
+import users.Users;
+
 public abstract class AbstractFactory {
+	
+	public abstract Users getUserType(String userType);
+	public abstract Sellable getSellableType(String sellableType);
+	public abstract TargetProfitPolicies getTargetProfitPoliciesType(String targetProfitPoliciesType);
+	public abstract DeliveryPolicies getDeliveryPoliciesType(String deliveryPoliciesType);
+	public abstract ShippedOrderSortingPolicies getSortingPoliciesType(String sortingPoliciesType);
+	public abstract FidelityCards getFidelityCardsType(String fidelityCardType);
+	
+	
+	
 	
 	
 
--- fr.cs.groupG.myFoodora/src/users/UsersFactory.java
+++ fr.cs.groupG.myFoodora/src/users/UsersFactory.java
@@ -1,8 +1,48 @@
 package users;
 
 import other.AbstractFactory;
+import other.DeliveryPolicies;
+import other.FidelityCards;
+import other.Sellable;
+import other.ShippedOrderSortingPolicies;
+import other.TargetProfitPolicies;
 
 public class UsersFactory extends AbstractFactory {
+	
+	
+	
+	
+	
+	
+	@Override
+	public Users getUserType(String userType) {
+		
+	}
+	
+	@Override
+	public Sellable getSellableType(String sellableType) {
+		return null;
+	}
+	
+	@Override 
+	public TargetProfitPolicies getTargetProfitPoliciesType(String targetProfitPoliciesType) {
+		return null;
+	}
+	
+	@Override 
+	public DeliveryPolicies getDeliveryPoliciesType(String deliveryPoliciesType) {
+		return null;
+	}
+	
+	@Override 
+	public ShippedOrderSortingPolicies getSortingPoliciesType(String sortingPoliciesType) {
+		return null;
+	}
+	
+	@Override 
+	public FidelityCards getFidelityCardsType(String fidelityCardType) {
+		return null;
+	}
 	
 	
 
